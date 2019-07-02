32986da54aac943c83c932099b6ad8c0757bcfa1
ender_xps13_idea <enderwsp@hotmail.com>
2019-07-02 16:02:02 +0800

    define the CustomerInfor create Data Tool

--- src/main/java/com/coding/sales/beans/CustomerInforBean.java
+++ src/main/java/com/coding/sales/beans/CustomerInforBean.java
@@ -1,5 +1,7 @@
 package com.coding.sales.beans;
 
+import com.coding.sales.enums.MemberType;
+
 /**
  * @author : wushnegping
  * @version V1.0
@@ -9,5 +11,40 @@ package com.coding.sales.beans;
  * @date Date : 2019年07月02日 15:41
  */
 public class CustomerInforBean {
-    
+    private String name;
+    private MemberType memberType;
+    private String acNo;
+    private int currentMemberPoints;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MemberType getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(MemberType memberType) {
+        this.memberType = memberType;
+    }
+
+    public String getAcNo() {
+        return acNo;
+    }
+
+    public void setAcNo(String acNo) {
+        this.acNo = acNo;
+    }
+
+    public int getCurrentMemberPoints() {
+        return currentMemberPoints;
+    }
+
+    public void setCurrentMemberPoints(int currentMemberPoints) {
+        this.currentMemberPoints = currentMemberPoints;
+    }
 }
---
+++ src/main/java/com/coding/sales/datas/CustomerInforDatas.java
@@ -0,0 +1,12 @@
+package com.coding.sales.datas;
+
+/**
+ * @author : wushnegping
+ * @version V1.0
+ * @Project: kata-precious_metal_sales_system
+ * @Package com.coding.sales.datas
+ * @Description: 客户信息列表，模拟数据库出库信息（有限数据建议加控制是否存在对于数据）
+ * @date Date : 2019年07月02日 15:55
+ */
+public class CustomerInforDatas {
+}
